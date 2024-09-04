501aa6a22d71ec48577a65526f50da415e60aa9d
a-j-sheilla <sheillajuma001@gmail.com>
2024-09-04 21:05:56 +0300

    comment on solving both parts of the question

--- day_4/stocking.go
+++ day_4/stocking.go
@@ -18,16 +18,18 @@ func main() {
         hash := md5.Sum([]byte(data))
         hashString := hex.EncodeToString(hash[:])
 		//fmt.Println(numString,secretKey,data,hash,hashString)
-        
-        // if hashString[:5] == "00000" {
-        //     fmt.Println("The lowest number to produce the hashstring:", i)
-        //     break
-        // }
+        //part 1 solution of day 4
+        if hashString[:5] == "00000" {
+            fmt.Println("The lowest number to produce the hashstring:", i)
+            break
+        }
+		// part 2 solution of day 4
 		 if hashString[:6] == "000000" {
 			fmt.Println("Starting with 6 zeroes is: ", i)
 			break
 
 		 }
+		 //********comment out part 1 solution to find answer for part 2**********
 
         i++
     }
